60cab0dcaeaa2e9e6924eadbc6a106bd2fca1b55
75u2u <75u2uuy4@gmail.com>
2020-11-02 22:14:42 +0900

    threejsテスト

--- index.js
+++ index.js
@@ -1,47 +1,16 @@
-window.addEventListener('DOMContentLoaded', init);
+var express = require('express'),
+    path = require('path'),
+    app = express();
 
-function init() {
-  const width = 960;
-  const height = 540;
 
-  // レンダラーを作成
-  const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#myCanvas')
-  });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(width, height);
+app.set('port', (process.env.PORT || 5000));
 
-  // シーンを作成
-  const scene = new THREE.Scene();
+app.use(express.static('build'));
 
-  // カメラを作成
-  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-  camera.position.set(0, 0, +1000);
+app.listen(app.get('port'), function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Running on port: ' + app.get('port')); }
+});
 
-  // 箱を作成
-  const geometry = new THREE.BoxGeometry(500, 500, 500);
-  const material = new THREE.MeshStandardMaterial({color: 0x0000FF});
-  const box = new THREE.Mesh(geometry, material);
-  scene.add(box);
-
-  // 平行光源
-  const light = new THREE.DirectionalLight(0xFFFFFF);
-  light.intensity = 2; // 光の強さを倍に
-  light.position.set(1, 1, 1);
-  // シーンに追加
-  scene.add(light);
-
-  // 初回実行
-  tick();
-
-  function tick() {
-    requestAnimationFrame(tick);
-
-    // 箱を回転させる
-    box.rotation.x += 0.01;
-    box.rotation.y += 0.01;
-
-    // レンダリング
-    renderer.render(scene, camera);
-  }
-}

ab7057bddd755bc54da4a61fa3dc2b192ad5333b
75u2u <75u2uuy4@gmail.com>
2020-11-07 21:14:22 +0900

    apache対応

--- build/index.php
+++ build/index.php
@@ -2,8 +2,8 @@
 require_once("Smarty/libs/Smarty.class.php");
 $smarty = new Smarty();
 
-$smarty->template_dir = "./templates/";
-$smarty->compile_dir = "./Smarty/templates_c/";
+$smarty->template_dir = "templates/";
+$smarty->compile_dir = "Smarty/templates_c/";
 
 $smarty->assign("time", "00:59");
 $smarty->assign("point", "334");
